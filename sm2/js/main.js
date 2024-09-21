ea642aa523cd6153a2cac7763e7ef206606279d1
Ахмедов Азамат Амирович <152305@mck-ktits.ru>
2024-09-21 09:31:45 +0300

    sm2 finished

--- sm2/js/main.js
+++ sm2/js/main.js
@@ -1,16 +1,14 @@
 let name = prompt("Как тебя зовут?");
-let age = prompt("Сколько теб лет?");
+let age = prompt("Сколько тебe лет?");
 if (age < 20) {
     alert("Тебе меньше 20");
 } else if (age > 20) {
-    alert("Привет");
-    alert("Привет");
-    alert("Привет");
-    alert("Привет");
-    alert("Привет");2
+    for (let index = 0; index < 5; index++) {
+        alert("hello")
+    }
 }
 
 let js = confirm("Хочешь учить js?");
-if (js == true) {
+if (js) {
     alert("Супер");
 }
