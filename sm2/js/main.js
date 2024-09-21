3ee841b3342f3ce8672d5e7ad60a0e4d5ebb2a32
Ахмедов Азамат Амирович <152305@mck-ktits.ru>
2024-09-21 09:01:36 +0300

    sm2, if else and alert/prompt

---
+++ sm2/js/main.js
@@ -0,0 +1,16 @@
+let name = prompt("Как тебя зовут?");
+let age = prompt("Сколько теб лет?");
+if (age < 20) {
+    alert("Тебе меньше 20");
+} else if (age > 20) {
+    alert("Привет");
+    alert("Привет");
+    alert("Привет");
+    alert("Привет");
+    alert("Привет");2
+}
+
+let js = confirm("Хочешь учить js?");
+if (js == true) {
+    alert("Супер");
+}
